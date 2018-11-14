873d87ae937489fca6984babe1f2370284ad2e29
Bergius Tanja <berta@haaga-helia.fi>
2018-11-14 10:09:51 +0200

    valmis ratkaisu

---
+++ src/main/java/fi/haagahelia/servercourse/web/HelloController.java
@@ -0,0 +1,20 @@
+package fi.haagahelia.servercourse.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+	@RequestMapping("/index")
+	public String indexPage() {
+		return "This is the main page";
+	}
+
+	@RequestMapping("/contact")
+	public String contactPage() {
+		return "This is the contact page";
+	}
+}
